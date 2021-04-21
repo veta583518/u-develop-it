179fb013a532b17d4edcf3f9ae8d7599bae889cc
CoCo Chanel <veta583518@gmail.com>
2021-04-21 00:39:11 -0500

    added routes for voters table, and modularized the routes and database

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,28 @@
+/* If table already exists delete it */
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS voters;
+/* Create parties table and columns*/
 CREATE TABLE parties (
     id INTEGER PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT
 );
+/* Create candidates table and columns*/
 CREATE TABLE candidates (
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL,
     party_id INTEGER UNSIGNED,
+    industry_connected BOOLEAN NOT NULL,
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE
     SET NULL
+);
+/* Create voters table and columns*/
+CREATE TABLE voters (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
